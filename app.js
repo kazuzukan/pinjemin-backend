8168653eddd4626024805e1e7fdef26622cb8380
kazuzukan <banghaji363@gmail.com>
2020-05-28 20:13:09 +0700

    reset database value

--- app.js
+++ app.js
@@ -17,14 +17,14 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require('./models');
-db.sequelize.sync().then(()=>{
-    console.log('Database Synced')
-})
+// const db = require('./models');
+// db.sequelize.sync().then(()=>{
+//     console.log('Database Synced')
+// })
 
-// db.sequelize.sync({ force: true }).then(() => {
-//     console.log("Drop and re-sync db.");
-// });
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+});
 
 const route = require('./routes/index');
 route(app);
